7b08f0b51968dc85ff22ee49f85f22a63887c17c
Abdurr <94680006+Lastdough@users.noreply.github.com>
2023-11-25 21:19:53 +0700

    [UPDATE] change mongo url

--- src/config/database.js
+++ src/config/database.js
@@ -6,8 +6,7 @@ const mongoose = require('mongoose');
 const database_url = 'mongodb://localhost:27017/db_nutrify';
 
 const database = mongoose.connect(
-  'mongodb://127.0.0.1:27017/db_nutrify'
+  `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@nutrify-server.muqa09o.mongodb.net/?retryWrites=true&w=majority`
 );
-// `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@nutrify-server.muqa09o.mongodb.net/?retryWrites=true&w=majority`
 
 module.exports = database;
