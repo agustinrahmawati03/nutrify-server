491b2f12ea1e043fd6070e35d674b9917b0e35f5
Abdurr <94680006+Lastdough@users.noreply.github.com>
2023-11-25 22:51:23 +0700

    [UPDATE] node env for prod and dev

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,8 @@ const mongoose = require('mongoose');
 const database_url = 'mongodb://localhost:27017/db_nutrify';
 
 const database = mongoose.connect(
-  `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@nutrify-server.muqa09o.mongodb.net/?retryWrites=true&w=majority`
+  (process.env.NODE_ENV === 'development') ? 'mongodb://127.0.0.1:27017/db_nutrify' : `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@nutrify-server.muqa09o.mongodb.net/?retryWrites=true&w=majority`
 );
+// 
 
 module.exports = database;
