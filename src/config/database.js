edb3c385fa73aec6124828e0ef0083fbf1485c90
Abdurr <94680006+Lastdough@users.noreply.github.com>
2023-11-25 17:52:18 +0700

    [CHANGE] bcrypt to bcryptjs

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { MONGODB_URI } = process.env;
+const { MONGODB_PASS, MONGODB_USER } = process.env;
 
 const mongoose = require('mongoose');
 
@@ -8,5 +8,6 @@ const database_url = 'mongodb://localhost:27017/db_nutrify';
 const database = mongoose.connect(
   'mongodb://127.0.0.1:27017/db_nutrify'
 );
+// `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@nutrify-server.muqa09o.mongodb.net/?retryWrites=true&w=majority`
 
 module.exports = database;
