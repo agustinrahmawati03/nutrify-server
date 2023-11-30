423752d7a9bf7977f9b99b3cc72d272d7054d525
rioaldie <rioaldierwanto@gmail.com>
2023-12-01 00:39:05 +0700

    fixing cors

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,16 +1,20 @@
 const express = require('express');
 
 const {
-    getUserProfile,
-    editUserProfile,
-    changeUserPassword,    
+  getUserProfile,
+  editUserProfile,
+  changeUserPassword,
 } = require('../controllers/user.controller');
-
+const { tokenVerified, forUser } = require('../middleware/token');
 
 const userRoute = express.Router();
 
-userRoute.get('/', getUserProfile);
-userRoute.put('/', editUserProfile);
-userRoute.put('/change-password', changeUserPassword);
+userRoute.get('/', [tokenVerified, forUser], getUserProfile);
+userRoute.put('/', [tokenVerified, forUser], editUserProfile);
+userRoute.put(
+  '/change-password',
+  [tokenVerified, forUser],
+  changeUserPassword
+);
 
 module.exports = userRoute;
