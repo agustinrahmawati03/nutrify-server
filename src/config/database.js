5f7f65dbd0f1a24506e0e0cf9cdba51128cdd968
Wildan M Zaki <wildanmzaki7@gmail.com>
2025-03-05 21:34:03 +0700

    change db config

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,13 @@ const { MONGODB_PASS, MONGODB_USER } = process.env;
 
 const mongoose = require('mongoose');
 
-const database_url = 'mongodb+srv://nutrify:nutrify@cluster0.s9j8c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const database_url =
+  'mongodb+srv://nutrify:nutrify@cluster0.s9j8c.mongodb.net/nutrisiku?retryWrites=true&w=majority&appName=Cluster0';
 
 const database = mongoose.connect(
-  (process.env.NODE_ENV === 'development') ? 'mongodb://127.0.0.1:27017/nutrify' : database_url
+  process.env.NODE_ENV === 'development'
+    ? 'mongodb://127.0.0.1:27017/nutrify'
+    : database_url
 );
 // const database = mongoose.connect ('mongodb://127.0.0.1:27017/nutrify');
 module.exports = database;
