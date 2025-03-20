0fdadaa221a2d3e4671e2ba09bfd3c6b4155bbd4
Agustin Rahmawati <rhmagustin08@gmail.com>
2025-03-20 21:13:03 +0700

    fix : only admin add foods

--- src/routes/food.router.js
+++ src/routes/food.router.js
@@ -6,12 +6,12 @@ const {
   getFoodByQuery,
   getFoodByID,
 } = require('../controllers/food.controller');
-const { tokenVerified, forUser } = require('../middleware/token');
+const { tokenVerified, forUser, onlyAdmin } = require('../middleware/token');
 
 const foodRoute = express.Router();
 
 foodRoute.get('/', getAllFoods);
-foodRoute.post('/', [tokenVerified, forUser], addManyFoods);
+foodRoute.post('/', [tokenVerified, onlyAdmin], addManyFoods);
 foodRoute.get('/search', [tokenVerified, forUser], getFoodByQuery);
 foodRoute.get('/:id', [tokenVerified, forUser], getFoodByID);
 foodRoute.get(
